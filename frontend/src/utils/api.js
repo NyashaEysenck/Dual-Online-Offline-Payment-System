791c4a426c0c1f282429057d8ae2b43e84034e30
Nyasha <159690091+NyashaEysenck@users.noreply.github.com>
2025-05-10 17:09:13 +0200

    Update api.js fix baseurl

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://192.168.131.181:5000/api',
+  baseURL: 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json',
   },
